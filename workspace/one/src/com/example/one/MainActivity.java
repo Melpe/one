4c0259cddf986b6ecb6ac1d3f24a169c8f601fb0
Melpe <palermemarcellino.pro@gmail.com>
2014-02-27 21:53:05 +0100

    action bouton
    texte du bouton change quand on clique dedans

--- workspace/one/src/com/example/one/MainActivity.java
+++ workspace/one/src/com/example/one/MainActivity.java
@@ -3,37 +3,27 @@ package com.example.one;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 
-public class MainActivity extends Activity {
-	private TextView coucou = null;
-	private RelativeLayout layout;
+public class MainActivity extends Activity implements View.OnTouchListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        coucou = new TextView(this);
-        coucou.setText("GO GO CakeOffTeam ");
-        coucou.setPadding(80, 50, 50, 80);
+        setContentView(R.layout.activity_main);
         
-        //recupere le layout directement dans le xml
-        layout = (RelativeLayout)RelativeLayout.inflate(this, R.layout.activity_main, null);
-        
-        //on recupere la vue text
-        coucou= (TextView)layout.findViewById(R.id.text );
-        // on lui change sont text
-        coucou.setText("Changer avant");
-        //on change sa position
-        coucou.setPadding(50,50,0,0);
-        
-        setContentView(layout);
-       
-        
-       
-        
+        TextView text = (TextView)findViewById(R.id.text);
+        text.setText("Cliquer sur le bouton ...");
+        Button bou = (Button)findViewById(R.id.button1);
+        bou.setTextSize(8);
+        bou.setOnTouchListener(this);
+
     }
 
 
@@ -42,6 +32,19 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    @Override
+    public boolean onTouch(View vue, MotionEvent event){
+    	
+    	Button b1 = (Button)vue;
+    	b1.setWidth((int)event.getX());
+    	b1.setHeight((int)event.getY());
+    	
+    	b1.setText("Votre position sur le bouton : X = " + event.getX() + "et Y= "+event.getY());
+    	
+    	
+    	return true;
     }
 
 }
